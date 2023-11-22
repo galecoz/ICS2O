7ade9bf8926a738842c7aa5ebb715f8119174206
galecoz <151762344+galecoz@users.noreply.github.com>
2023-11-22 10:32:09 -0500

    First Commit

---
+++ Unit 4/Unit 4 quiz asignment/Unit 4 Summative quiz assignment.py
@@ -0,0 +1,25 @@
+"""
+Name: Gabriel Dominato
+Date: ------
+Version: 1.0
+Discription: Create a quiz program that Contains 6 questions, 4 multiple choice (2 ordinary and 2 hard with 2 trys),
+and 2 fill in the blanks that accepts upper and lower case
+"""
+
+import time, random
+
+#Variables
+quizRun = True
+
+
+
+
+
+
+#Intro To User
+print("Hello User. Welcome to the --- quiz.")
+print("There will be a Total of 6 questions with 4 multiple choice and 2 fill in the blanks. 2 of the multiple choice questions will be single attempt and 2 will have 2 attemps>.")
+
+
+#Program
+#while quizRun = True:
