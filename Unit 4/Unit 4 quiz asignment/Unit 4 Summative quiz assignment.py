a8f2b2e8231a003685464d9b334c9604b3f2d3ab
galecoz <151762344+galecoz@users.noreply.github.com>
2023-11-22 11:18:23 -0500

    quiz

--- Unit 4/Unit 4 quiz asignment/Unit 4 Summative quiz assignment.py
+++ Unit 4/Unit 4 quiz asignment/Unit 4 Summative quiz assignment.py
@@ -8,19 +8,37 @@ and 2 fill in the blanks that accepts upper and lower case
 
 import time, random
 
-#Variables
+
+#?Variables
 quizRun = True
+name = ""
+response = ""
+responses = []
+    
 
 
+#^Intro To User
 
-
-
-
-#Intro To User
 print("Hello User. Welcome to the --- quiz.")
 print("There will be a Total of 6 questions with 4 multiple choice and 2 fill in the blanks. 2 of the multiple choice questions will be single attempt and 2 will have 2 attemps>.")
+print("-"*30)
 
-
-#Program
+#^Program
 while quizRun == True:
+    name = input("Name: ")
+    time.sleep(.25)
     print("")
+    print("Question 1: ")
+    print("1. ")
+    print("2. ")
+    print("3. ")
+    print("4. ")
+    print("5. ")
+    
+    while response != "1" or "2" or "3" or "4" or "5":
+        response = int(input("Answer: "))
+    responses.append(response)
+
+    
+    
+    
